9a0b8d0d435c052cfa7d837ea050d7f96d239837
rcarnold <rcarnold@bbw.com>
2023-10-03 12:42:34 -0400

    first commit

---
+++ src/HealthCheckServlet.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HealthCheckServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().write("OK");
+    }
+}
